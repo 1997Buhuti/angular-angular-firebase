f869b85a053816bc3783751ba55cb6afa73c2d58
Manakal <dp.manakal82@gmail.com>
2022-01-24 17:27:26 +0530

    initialized firebase app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'fire-base-learn-9a4db',
+    appId: '1:367723624399:web:a97124c7a3d595e46844ba',
+    databaseURL: 'https://fire-base-learn-9a4db-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'fire-base-learn-9a4db.appspot.com',
+    apiKey: 'AIzaSyASLbh_jRV1pQL7OY1H0KVaOX7Z3J61bi4',
+    authDomain: 'fire-base-learn-9a4db.firebaseapp.com',
+    messagingSenderId: '367723624399',
+    measurementId: 'G-MEPDVEJGV9',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyASLbh_jRV1pQL7OY1H0KVaOX7Z3J61bi4',
+    authDomain: 'fire-base-learn-9a4db.firebaseapp.com',
+    databaseURL:
+      'https://fire-base-learn-9a4db-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'fire-base-learn-9a4db',
+    storageBucket: 'fire-base-learn-9a4db.appspot.com',
+    messagingSenderId: '367723624399',
+    appId: '1:367723624399:web:a97124c7a3d595e46844ba',
+    measurementId: 'G-MEPDVEJGV9',
+  },
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
